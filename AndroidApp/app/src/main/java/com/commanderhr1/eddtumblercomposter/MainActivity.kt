bd362bf186d4322e130cad83501b55794f84a438
Harris Ransom <hransom528@gmail.com>
2022-03-15 10:38:50 -0400

    Added AndroidApp in Kotlin

---
+++ AndroidApp/app/src/main/java/com/commanderhr1/eddtumblercomposter/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.commanderhr1.eddtumblercomposter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
