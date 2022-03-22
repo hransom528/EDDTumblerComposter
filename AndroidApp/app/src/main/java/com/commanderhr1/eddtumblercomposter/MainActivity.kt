b990c526735a6218a8fbd3ebaf117e895e82729f
Harris Ransom <hransom528@gmail.com>
2022-03-22 14:26:03 -0400

    Developed WiFi and Bluetooth code for Android App

--- AndroidApp/app/src/main/java/com/commanderhr1/eddtumblercomposter/MainActivity.kt
+++ AndroidApp/app/src/main/java/com/commanderhr1/eddtumblercomposter/MainActivity.kt
@@ -6,19 +6,41 @@ import android.os.Bundle
 import android.bluetooth.*
 import android.content.Context
 import android.net.wifi.*
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
+import android.widget.Switch
 
 
 class MainActivity : AppCompatActivity() {
+    // WiFi
+    private val wifi : WifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
 
+    // Bluetooth
+    private val bluetoothManager : BluetoothManager = applicationContext.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+    private val bluetoothAdapter : BluetoothAdapter = bluetoothManager.adapter
+    protected val bleScanner = bluetoothAdapter.bluetoothLeScanner
+    private var scanning = false
+    private val SCAN_PERIOD: Long = 10000
+    private var handler = Handler(Looper.getMainLooper())
+
+    // Buttons
+    private val btnConnect: Button = findViewById(R.id.btnConnect)
+    private val swtWifiOn : Switch = findViewById(R.id.swtWifiOn)
+
+    // onCreate
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val wifi : WifiManager = getSystemService(Context.WIFI_SERVICE) as WifiManager
+
+        // TODO: Update preliminary Bluetooth code
+
+
         // TODO: Update preliminary WiFi code
         //val wifiSpecifier : WifiNetworkSpecifier =   WifiNetworkSpecifier.Builder
-        wifi.setWifiEnabled(true);
-        val btnConnect = findViewById<Button>(R.id.btnConnect)
+        //wifi.isWifiEnabled = true
+        wifi.isEasyConnectSupported
+
 
         // btnConnect
         btnConnect.setOnClickListener {
@@ -26,6 +48,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
-
+    // Scans for BLE devices
+    /*private fun scanBLEDevice() {
+        if (!scanning) { // Stops scanning after a pre-defined scan period.
+            handler.postDelayed({
+                scanning = false
+                bleScanner.stopScan(leScanCallback)
+            }, SCAN_PERIOD)
+            scanning = true
+            bleScanner.startScan(leScanCallback)
+        } else {
+            scanning = false
+            bleScanner.stopScan(leScanCallback)
+        }
+    }*/
 }
