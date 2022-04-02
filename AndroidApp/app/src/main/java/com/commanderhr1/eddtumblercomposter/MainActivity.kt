ddf72a2587ff43b867272f0912e3f211969095f5
Harris Ransom <hransom528@gmail.com>
2022-04-02 00:08:28 -0400

    Debugged BT initialization code and updated UI

--- AndroidApp/app/src/main/java/com/commanderhr1/eddtumblercomposter/MainActivity.kt
+++ AndroidApp/app/src/main/java/com/commanderhr1/eddtumblercomposter/MainActivity.kt
@@ -7,6 +7,7 @@ package com.commanderhr1.eddtumblercomposter
 //import android.os.Handler
 //import android.os.Looper
 
+import android.Manifest.permission.BLUETOOTH_CONNECT
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
@@ -17,59 +18,67 @@ import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
 import androidx.core.app.ActivityCompat
 import java.util.*
 
-
+// MainActivity
 class MainActivity : AppCompatActivity() {
-    // WiFi
-    private val wifi : WifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-
-    // Bluetooth
-    private val bluetoothManager : BluetoothManager = applicationContext.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-    private val bluetoothAdapter : BluetoothAdapter? = bluetoothManager.adapter
-    private val REQUEST_ENABLE_BT = 1
-    // TODO: Generate UUID for Bluetooth service
-    private val APP_UUID = UUID.randomUUID();
-
-    // BLE
-    /*protected val bleScanner = bluetoothAdapter.bluetoothLeScanner
-    private var scanning = false
-    private val SCAN_PERIOD: Long = 10000
-    private var handler = Handler(Looper.getMainLooper())*/
-
-    // Buttons
-    private val btnWifiConnect: Button = findViewById(R.id.btnWifiConnect)
-    private val swtWifiOn : SwitchCompat = findViewById(R.id.swtWifiOn)
-    private val swtBTOn : SwitchCompat = findViewById(R.id.swtBTOn)
+    //
+    private val LOG_TAG : String = "AppMain"
 
     // onCreate
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Logging and Alerts
+
+        // UI
+        val btnRequestWifiPerms : Button = findViewById(R.id.btnRequestWifiPerms)
+        val btnRequestBTPerms : Button = findViewById(R.id.btnRequestBTPerms)
+        val btnWifiConnect: Button = findViewById(R.id.btnWifiConnect)
+        val swtWifiOn : SwitchCompat = findViewById(R.id.swtWifiOn)
+        val swtBTOn : SwitchCompat = findViewById(R.id.swtBTOn)
+
+
+        // BLE
+        /*protected val bleScanner = bluetoothAdapter.bluetoothLeScanner
+        private var scanning = false
+        private val SCAN_PERIOD: Long = 10000
+        private var handler = Handler(Looper.getMainLooper())*/
+
+        // WiFi initialization
+        val wifi : WifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+
+        // Bluetooth initialization
+        val bluetoothManager : BluetoothManager = applicationContext.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        val bluetoothAdapter : BluetoothAdapter? = bluetoothManager.adapter
+        val REQUEST_ENABLE_BT = 1
+        val APP_UUID = UUID.fromString("7f27fb35-8295-4755-8596-236b479f1ff0")
+
+
         // TODO: Update Bluetooth code with server functionality
         if (bluetoothAdapter == null) {
             // Device doesn't support bluetooth
+            Log.e(LOG_TAG, "Device doesn't support bluetooth")
         }
 
         // Request Bluetooth permission if not enabled
+        checkBluetoothConnectPermission(BLUETOOTH_CONNECT)
         if (!bluetoothAdapter?.isEnabled!!) {
             val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
         }
 
-        // Checks connect permission for Bluetooth
-        checkBluetoothConnectPermission()
-
-        // Checks app permissions for BLUETOOTH_CONNECT and notifies user if there is a permission mismatch
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(this, "First enable Bluetooth Access in Settings!", Toast.LENGTH_LONG).show()
-        }
+        // Checks BLUETOOTH_CONNECT permission for Bluetooth
+        checkBluetoothConnectPermission(BLUETOOTH_CONNECT)
 
         // Queries list of previously paired devices
         val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter.bondedDevices
@@ -83,12 +92,12 @@ class MainActivity : AppCompatActivity() {
         registerReceiver(receiver, filter)
 
         // Enables Bluetooth discoverability
-        val requestCode = 1;
+        val requestCode = 1
         val discoverableIntent: Intent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).putExtra(
             BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300)
         startActivityForResult(discoverableIntent, requestCode)
 
-        //
+        // Starts bluetooth scanning
         bluetoothAdapter.listenUsingRfcommWithServiceRecord("EDDTumblerComposterApp", APP_UUID)
 
         // TODO: Update preliminary WiFi code
@@ -96,6 +105,16 @@ class MainActivity : AppCompatActivity() {
         //wifi.isWifiEnabled = true
         val easyConnect = wifi.isEasyConnectSupported
 
+        // TODO: Implement permission checking inside button onClickListener methods
+        // btnRequestWifiPerms
+        btnRequestWifiPerms.setOnClickListener {
+
+        }
+
+        // btnRequestBTPerms
+        btnRequestBTPerms.setOnClickListener {
+
+        }
 
         // btnConnect
         btnWifiConnect.setOnClickListener {
@@ -112,7 +131,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        // swtBTOn
+        // swtWiFiOn
         swtWifiOn.setOnCheckedChangeListener { _, isChecked ->
             wifi.isWifiEnabled = isChecked
         }
@@ -124,13 +143,34 @@ class MainActivity : AppCompatActivity() {
 
         // Don't forget to unregister the ACTION_FOUND receiver.
         unregisterReceiver(receiver)
+
     }
 
     // Checks for BLUETOOTH_CONNECT permission
-    private fun checkBluetoothConnectPermission() {
+    private fun checkBluetoothConnectPermission(permission : String) {
         // Checks app permissions for BLUETOOTH_CONNECT and notifies user if there is a permission mismatch
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(this, "First enable Bluetooth Access in Settings!", Toast.LENGTH_LONG).show()
+        if (ActivityCompat.checkSelfPermission(this, BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            val requestPermissionLauncher =
+                registerForActivityResult(RequestPermission()) { isGranted: Boolean ->
+                    if (isGranted) {
+                        // Permission is granted. Continue the action
+
+                    } else {
+                        // Explain to the user that the feature is unavailable because the
+                        // features requires a permission that the user has denied. At the
+                        // same time, respect the user's decision. Don't link to system
+                        // settings in an effort to convince the user to change their
+                        // decision.
+                            // TODO: Update alertDialog for BT perms
+                        val btDialogBuilder = AlertDialog.Builder(this)
+                        btDialogBuilder.setMessage("First enable Bluetooth Access!")
+                        val btAlertDialog = btDialogBuilder.create()
+                        btAlertDialog.show()
+                        Toast.makeText(this, "First enable Bluetooth Access!", Toast.LENGTH_LONG).show()
+                        Log.w(LOG_TAG, "User permission for $permission not granted")
+                    }
+                }
+            requestPermissionLauncher.launch(permission)
         }
     }
 
@@ -143,7 +183,7 @@ class MainActivity : AppCompatActivity() {
                     // object and its info from the Intent.
                     val device: BluetoothDevice =
                         intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)!!
-                    checkBluetoothConnectPermission()
+                    checkBluetoothConnectPermission(BLUETOOTH_CONNECT)
                     val deviceName = device.name
                     val deviceHardwareAddress = device.address // MAC address
                 }
