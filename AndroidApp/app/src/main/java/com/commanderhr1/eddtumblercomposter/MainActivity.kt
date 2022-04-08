8839ebb42b4f43bc5fc249eb2eda567fd3f95674
Harris Ransom <hransom528@gmail.com>
2022-04-08 13:21:22 -0400

    Cleaned up Android App code

--- AndroidApp/app/src/main/java/com/commanderhr1/eddtumblercomposter/MainActivity.kt
+++ AndroidApp/app/src/main/java/com/commanderhr1/eddtumblercomposter/MainActivity.kt
@@ -5,10 +5,7 @@ package com.commanderhr1.eddtumblercomposter
 //import android.Manifest
 //import android.app.Activity
 
-//import android.bluetooth.le.ScanCallback
-
-import android.Manifest.permission.BLUETOOTH_CONNECT
-import android.bluetooth.*
+import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -33,21 +30,14 @@ import androidx.core.app.ActivityCompat
 import java.io.*
 import java.net.InetAddress
 import java.net.Socket
-import java.util.*
 
 // MainActivity
 class MainActivity : AppCompatActivity() {
     // Logging
     private val LOG_TAG : String = "AppMain"
 
-    // Bluetooth
-    private lateinit var bluetoothManager : BluetoothManager
-    private lateinit var bluetoothAdapter : BluetoothAdapter
-    val APP_UUID: UUID = UUID.fromString("7f27fb35-8295-4755-8596-236b479f1ff0")
-
     // WiFi
     private var socket : Socket? = null
-
 
     // onCreate
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,22 +46,15 @@ class MainActivity : AppCompatActivity() {
 
         // UI
         val btnRequestWifiPerms : Button = findViewById(R.id.btnRequestWifiPerms)
-        val btnRequestBTPerms : Button = findViewById(R.id.btnRequestBTPerms)
         val btnWifiConnect: Button = findViewById(R.id.btnWifiConnect)
         val swtWifiOn : SwitchCompat = findViewById(R.id.swtWifiOn)
-        val swtBTOn : SwitchCompat = findViewById(R.id.swtBTOn)
 
         // Shared preferences
         val examplePrefs = getSharedPreferences("PREFS", 0)
         val editor = examplePrefs.edit()
 
-        // BLE
-        /*protected val bleScanner = bluetoothAdapter.bluetoothLeScanner
-        private var scanning = false
-        private val SCAN_PERIOD: Long = 10000
-        private var handler = Handler(Looper.getMainLooper())*/
-
         // WiFi initialization
+        checkWifiPermission(ACCESS_FINE_LOCATION)
         val wifi : WifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
         val wifiList = wifi.configuredNetworks
         val networkSSID = "EDDTumblerComposter"
@@ -85,36 +68,6 @@ class MainActivity : AppCompatActivity() {
         }
         val wifiP2PChannel : WifiP2pManager.Channel =  wifiP2P.initialize(applicationContext, looper, p2pChannelListener)
 
-
-        // Bluetooth initialization
-        bluetoothManager = applicationContext.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-        bluetoothAdapter = bluetoothManager.adapter
-        val REQUEST_ENABLE_BT = 1
-
-        // Checks BLUETOOTH_CONNECT permission for Bluetooth
-        checkBluetoothConnectPermission(BLUETOOTH_CONNECT)
-
-        // Queries list of previously paired devices
-        val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter.bondedDevices
-        pairedDevices?.forEach { device ->
-            val deviceName = device.name
-            val deviceHardwareAddress = device.address // MAC Address
-
-            // Bypass scanning, go right to connection
-            if (deviceName.equals("EDDTumblerComposter")) {
-                // TODO: Implement scanning bypass
-            }
-        }
-
-
-        // Register for broadcasts when a device is discovered.
-        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
-        registerReceiver(receiver, filter)
-
-        // Starts bluetooth scanning
-        //AcceptThread().run()
-
-
         // TODO: Update WiFi scanning and connection code
         val wifiSpecifierBuilder : WifiNetworkSpecifier.Builder = WifiNetworkSpecifier.Builder()
         wifiSpecifierBuilder.setSsid(networkSSID)
@@ -122,13 +75,11 @@ class MainActivity : AppCompatActivity() {
         val wifiSpecifier : WifiNetworkSpecifier = wifiSpecifierBuilder.build()
 
         // WifiConfiguration implementation, deprecated but should work
-        val wifiConf : WifiConfiguration = WifiConfiguration()
-        wifiConf.SSID = "\"" + networkSSID + "\"";   // Please note the quotes. String should contain ssid in quotes
-        wifiConf.preSharedKey = "\""+ networkPass +"\"";
+        val wifiConf = WifiConfiguration()
+        wifiConf.SSID = "\"" + networkSSID + "\""   // Please note the quotes. String should contain ssid in quotes
+        wifiConf.preSharedKey = "\""+ networkPass +"\""
         val netID : Int = wifi.addNetwork(wifiConf)
-        wifi.disconnect()
-        wifi.enableNetwork(netID, true)
-        wifi.reconnect()
+
 
         // WifiNetworkSuggestion implementation
         val wifiNetworkSuggestionBuilder : WifiNetworkSuggestion.Builder = WifiNetworkSuggestion.Builder()
@@ -169,7 +120,7 @@ class MainActivity : AppCompatActivity() {
         applicationContext.registerReceiver(wifiScanReceiver, wifiIntentFilter)
 
         // Starts WiFi scanning
-        var wifiScanSuccess : Boolean = wifi.startScan()
+        val wifiScanSuccess : Boolean = wifi.startScan()
         if (!wifiScanSuccess) {
             scanFailure(wifi)
         }
@@ -178,12 +129,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         // TCP client connection
-        val ipAddr : String = "192.168.4.1"
-        val wifiPort : Int = 80
-        var tcpClient : TcpClient
+        val ipAddr = "192.168.4.1"
+        val wifiPort = 80
+        //var tcpClient : TcpClient
         socket = Socket(ipAddr, wifiPort)
-        var socketInputStream : InputStream = socket!!.getInputStream()
-        var socketOutputStream : OutputStream = socket!!.getOutputStream()
 
         // TODO: Implement permission checking inside button onClickListener methods
         // btnRequestWifiPerms
@@ -191,35 +140,11 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        // btnRequestBTPerms
-        btnRequestBTPerms.setOnClickListener {
-            // Request BLUETOOTH_CONNECT permission
-            checkBluetoothConnectPermission(BLUETOOTH_CONNECT)
-            if (!bluetoothAdapter.isEnabled) {
-                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
-            }
-
-            // Enables Bluetooth discoverability
-            val requestCode = 1
-            val discoverableIntent: Intent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).putExtra(
-                BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300)
-            startActivityForResult(discoverableIntent, requestCode)
-        }
-
         // btnConnect
         btnWifiConnect.setOnClickListener {
-
-        }
-
-        // swtBTOn
-        swtBTOn.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked){
-                bluetoothAdapter.enable()
-            }
-            else {
-                bluetoothAdapter.disable()
-            }
+            wifi.disconnect()
+            wifi.enableNetwork(netID, true)
+            wifi.reconnect()
         }
 
         // swtWiFiOn
@@ -242,14 +167,13 @@ class MainActivity : AppCompatActivity() {
         socket?.close()
 
         // Don't forget to unregister the ACTION_FOUND receiver.
-        unregisterReceiver(receiver)
         unregisterReceiver(wifiScanReceiver)
     }
 
     // Checks for BLUETOOTH_CONNECT permission
-    private fun checkBluetoothConnectPermission(permission : String) {
+    private fun checkWifiPermission(permission : String) {
         // Checks app permissions for BLUETOOTH_CONNECT and notifies user if there is a permission mismatch
-        if (ActivityCompat.checkSelfPermission(this, BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
             val requestPermissionLauncher =
                 registerForActivityResult(RequestPermission()) { isGranted: Boolean ->
                     if (isGranted) {
@@ -261,12 +185,12 @@ class MainActivity : AppCompatActivity() {
                         // same time, respect the user's decision. Don't link to system
                         // settings in an effort to convince the user to change their
                         // decision.
-                        // TODO: Update alertDialog for BT perms
-                        val btDialogBuilder = AlertDialog.Builder(this)
-                        btDialogBuilder.setMessage("First enable Bluetooth Access!")
-                        val btAlertDialog = btDialogBuilder.create()
-                        btAlertDialog.show()
-                        Toast.makeText(this, "First enable Bluetooth Access!", Toast.LENGTH_LONG).show()
+                        // TODO: Update alertDialog for WiFi perms
+                        val wifiDialogBuilder = AlertDialog.Builder(this)
+                        wifiDialogBuilder.setMessage("First enable Wifi permissions!")
+                        val wifiAlertDialog = wifiDialogBuilder.create()
+                        wifiAlertDialog.show()
+                        Toast.makeText(this, "First enable Wifi permissions!", Toast.LENGTH_LONG).show()
                         Log.w(LOG_TAG, "User permission for $permission not granted")
                     }
                 }
@@ -274,22 +198,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    // Creates a BroadcastReceiver for ACTION_FOUND.
-    private val receiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            when(intent.action.toString()) {
-                BluetoothDevice.ACTION_FOUND -> {
-                    // Discovery has found a device. Get the BluetoothDevice
-                    // object and its info from the Intent.
-                    val device: BluetoothDevice =
-                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)!!
-                    checkBluetoothConnectPermission(BLUETOOTH_CONNECT)
-                    val deviceName = device.name
-                    val deviceHardwareAddress = device.address // MAC address
-                }
-            }
-        }
-    }
 
     // Creates a BroadcastReceiver for WiFi scan.
     private val wifiScanReceiver = object : BroadcastReceiver() {
@@ -304,125 +212,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    // TODO: Update Bluetooth server functionality
-    // Bluetooth connection/scanning thread
-    /*
-    @SuppressLint("MissingPermission")
-    private inner class AcceptThread : Thread() {
-        private val mmServerSocket: BluetoothServerSocket? by lazy(LazyThreadSafetyMode.NONE) {
-            bluetoothAdapter.listenUsingInsecureRfcommWithServiceRecord("EDDTumblerComposterApp", APP_UUID)
-        }
-
-        override fun run() {
-            // Keep listening until exception occurs or a socket is returned.
-            var shouldLoop = true
-            while (shouldLoop) {
-                val socket: BluetoothSocket? = try {
-                    mmServerSocket?.accept()
-                } catch (e: IOException) {
-                    Log.e(LOG_TAG, "Socket's accept() method failed", e)
-                    shouldLoop = false
-                    null
-                }
-                socket?.also {
-                    //manageMyConnectedSocket(it)
-                    mmServerSocket?.close()
-                    shouldLoop = false
-                }
-            }
-        }
-
-        // Closes the connect socket and causes the thread to finish.
-        fun cancel() {
-            try {
-                mmServerSocket?.close()
-            } catch (e: IOException) {
-                Log.e(LOG_TAG, "Could not close the connect socket", e)
-            }
-        }
-    }*/
-
-    /*
-    // Bluetooth service for composter
-    class BluetoothService(
-        // handler that gets info from Bluetooth service
-        private val handler: Handler,
-        val TAG : String
-    ) {
-        private inner class ConnectedThread(private val mmSocket: BluetoothSocket) : Thread() {
-            private val mmInStream: InputStream = mmSocket.inputStream
-            private val mmOutStream: OutputStream = mmSocket.outputStream
-            private val mmBuffer: ByteArray = ByteArray(1024) // mmBuffer store for the stream
-
-            // Defines several constants used when transmitting messages between the
-            // service and the UI.
-            val MESSAGE_READ: Int = 0
-            val MESSAGE_WRITE: Int = 1
-            val MESSAGE_TOAST: Int = 2
-
-            override fun run() {
-                var numBytes: Int // bytes returned from read()
-
-                // Keep listening to the InputStream until an exception occurs.
-                while (true) {
-                    // Read from the InputStream.
-                    numBytes = try {
-                        mmInStream.read(mmBuffer)
-                    } catch (e: IOException) {
-                        Log.d(TAG, "Input stream was disconnected", e)
-                        break
-                    }
-
-                    // Send the obtained bytes to the UI activity.
-                    val readMsg = handler.obtainMessage(
-                        MESSAGE_READ, numBytes, -1,
-                        mmBuffer)
-                    readMsg.sendToTarget()
-                }
-            }
-
-            // Call this from the main activity to send data to the remote device.
-            fun write(bytes: ByteArray) {
-                try {
-                    mmOutStream.write(bytes)
-                } catch (e: IOException) {
-                    Log.e(TAG, "Error occurred when sending data", e)
-
-                    // Send a failure message back to the activity.
-                    val writeErrorMsg = handler.obtainMessage(MESSAGE_TOAST)
-                    val bundle = Bundle().apply {
-                        putString("toast", "Couldn't send data to the other device")
-                    }
-                    writeErrorMsg.data = bundle
-                    handler.sendMessage(writeErrorMsg)
-                    return
-                }
-
-                // Share the sent message with the UI activity.
-                val writtenMsg = handler.obtainMessage(
-                    MESSAGE_WRITE, -1, -1, mmBuffer)
-                writtenMsg.sendToTarget()
-            }
-
-            // Call this method from the main activity to shut down the connection.
-            fun cancel() {
-                try {
-                    mmSocket.close()
-                } catch (e: IOException) {
-                    Log.e(TAG, "Could not close the connect socket", e)
-                }
-            }
-        }
-    }*/
-
-
     // WiFi scan successful
     private fun scanSuccess(wifiManager : WifiManager) {
         val results = wifiManager.scanResults
 
+        Log.i(LOG_TAG, "WiFi Networks Successfully Scanned")
+
         //... use new scan results ...
         for (result in results) {
             if (result.SSID.equals("EDDTumblerComposter")) {
+                Log.i(LOG_TAG, "EDDTumblerComposter Detected")
                 // TODO: Implement WiFi connection
                 wifiConnectComposter(wifiManager)
             }
@@ -441,21 +240,6 @@ class MainActivity : AppCompatActivity() {
     private fun wifiConnectComposter(wifiManager: WifiManager) {
 
     }
-
-    // Scans for BLE devices
-    /*private fun scanBLEDevice() {
-        if (!scanning) { // Stops scanning after a pre-defined scan period.
-            handler.postDelayed({
-                scanning = false
-                bleScanner.stopScan(leScanCallback)
-            }, SCAN_PERIOD)
-            scanning = true
-            bleScanner.startScan(leScanCallback)
-        } else {
-            scanning = false
-            bleScanner.stopScan(leScanCallback)
-        }
-    }*/
 
     class TcpClient(listener: OnMessageReceived?) {
         // message to send to the server
@@ -564,6 +348,29 @@ class MainActivity : AppCompatActivity() {
          */
         init {
             mMessageListener = listener
+        }
+    }
+
+    //Declare the interface. The method messageReceived(String message) will must be implemented in the Activity
+    //class at on AsyncTask doInBackground
+    interface OnMessageReceived {
+        fun messageReceived(message: String?)
+    }
+
+    // TODO: Update TCP thread
+    // Thread for handling TCP I/O
+    fun tcpThread(start: Boolean = true,
+                  isDaemon: Boolean = false,
+                  contextClassLoader: ClassLoader? = null,
+                  name: String? = null,
+                  priority: Int = -1,
+                  block: () -> Unit, ) {
+        var socketInputStream : InputStream = socket!!.getInputStream()
+        var socketOutputStream : OutputStream = socket!!.getOutputStream()
+
+        // Data available on TCP socket
+        if (socketInputStream.available() > 0) {
+            val inputString : String = socketInputStream.read().toString()
         }
     }
 }
